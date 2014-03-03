7601fc66f8e2e1cf9f099623482f4034d29ff4ef
Max Klein <pcmaka@gmail.com>
2014-03-03 11:14:49 +0100

    Now supports old Facebook Chat-Style

--- data/facebook.js
+++ data/facebook.js
@@ -9,10 +9,11 @@ newMessages = function(){
 		var options = $(this).find("a[aria-label='Optionen']").next().find(".uiMenuInner");
 		if(!options.find("a[id='"+currentToDecryptId+"crypto']").length){
 			var addPubKeyHTML = "<li class='uiMenuItem'><a class='itemAnchor' role='menuitem' tabindex='0' id='"+currentToDecryptId+"crypto'><span class='itemLabel fsm'>Public Key eingeben</span></a></li>";
+			var clearKeysHTML = "<li class='uiMenuItem'><a class='itemAnchor' role='menuitem' tabindex='0' id='"+currentToDecryptId+"remove'><span class='itemLabel fsm' >Public Key löschen</span></a></li>";
 			var ownPrivKeyHTML = "<li class='uiMenuItem'><a class='itemAnchor' role='menuitem' tabindex='0' id='"+currentToDecryptId+"privkey'><span class='itemLabel fsm' >Eigener Private Key</span></a></li>";
 			var ownPublicKeyHTML = "<li class='uiMenuItem'><a class='itemAnchor' role='menuitem' tabindex='0' id='"+currentToDecryptId+"pubkey'><span class='itemLabel fsm' >Eigener Public Key</span></a></li>";
 			var trenner = "<li class='uiMenuSeparator'></li>";
-			options.html(addPubKeyHTML + ownPrivKeyHTML + ownPublicKeyHTML + trenner + options.html());
+			options.html(addPubKeyHTML + ownPrivKeyHTML + ownPublicKeyHTML + clearKeysHTML + trenner + options.html());
 			options.find("a[id='"+currentToDecryptId+"crypto']").click(function(){
 				publicKeyPrompt("Bitte Public Key für User "+currentToDecryptId+" einfügen:");
 			});
@@ -21,8 +22,16 @@ newMessages = function(){
 				window.alert(privKey);
 			});
 			options.find("a[id='"+currentToDecryptId+"pubkey']").click(function(){
-				var pubKey = NSAcrypt.getPublicKey(ownID)
+				var pubKey = NSAcrypt.getPublicKey(ownID);
 				window.alert(pubKey);
+			});
+			options.find("a[id='"+currentToDecryptId+"remove']").click(function(){
+				var removed = NSAcrypt.removePublicKey(currentToDecryptId);
+				if(removed){
+					window.alert("Removed");
+				}else{
+					window.alert("Kein Key vorhanden");
+				}
 			});
 		}
 		var pubkey = NSAcrypt.getPublicKey(currentToDecryptId); 
@@ -37,14 +46,26 @@ newMessages = function(){
 				window.alert(NSAcrypt.getPublicKey(currentToDecryptId));
 			});
 			$(this).find(".uiTextareaAutogrow._552m").css("border-color","red").css("border-style","groove").css("border-width","1px");
-			//$(this).find("._kso.fsm.direction_ltr._55r0").each(function(){
+			
+			//new CSS
 			$(this).find("span._5yl5").each(function(){
 				var textfield = $(this).children("span");
 				var text = textfield.text();
 				if (text.substring(0, 27) == "-----BEGIN PGP MESSAGE-----") {
 					decrypted = NSAcrypt.decrypt(text+"\n");
 					textfield.text(decrypted);
-					textfield.css("border-right","3px solid yellow");
+					textfield.closest("div._5w1r._5wdf").css("border","1px solid yellow");
+				}
+			});
+			
+			//old CSS
+			$(this).find("._kso.fsm.direction_ltr._55r0").each(function(){
+				var textfield = $(this).children("span").children("p");
+				var text = textfield.text();
+				if (text.substring(0, 27) == "-----BEGIN PGP MESSAGE-----") {
+					decrypted = NSAcrypt.decrypt(text+"\n");
+					textfield.text(decrypted);
+					textfield.css("border","1px solid yellow");
 				}
 			});
 		}
@@ -54,7 +75,6 @@ newMessages = function(){
 		var currentToDecryptId = 0;
 		$("a[data-hovercard*='/ajax/hovercard/hovercard.php?id=']").each(function(){
 			if($(this).closest("span")){
-				//TODO: PRÜFEN, OB IMMER KORREKT!
 				currentToDecryptId = $(this).attr("data-hovercard").split("=")[1].split("&")[0];
 				return false;
 			}
@@ -73,18 +93,6 @@ newMessages = function(){
 
 publicKeyPrompt = function (text){
 	self.port.emit("show",text);
-	/**var keyString = window.prompt(text);
-	if(keyString){
-		if (keyString.substring(0, 36) == "-----BEGIN PGP PUBLIC KEY BLOCK-----") {
-			if(!NSAcrypt.addPublicKey(keyString)){
-				publicKeyPrompt("Fehlerhafter Key!\n"+text);
-			}else{
-				window.alert("Public Key gespeichert!");
-			}
-		}else{
-			publicKeyPrompt("Fehlerhafter Key!\n"+text);
-		}
-	}**/
 }
 
 self.port.on("okKey",function(keyString){
@@ -93,6 +101,7 @@ self.port.on("okKey",function(keyString){
 			if(!NSAcrypt.addPublicKey(keyString)){
 				publicKeyPrompt("Fehlerhafter Key! Bitte erneut einfügen!");
 			}else{
+				self.port.emit("publicKey",keyString);
 				window.alert("Public Key gespeichert!");
 			}
 		}else{
@@ -103,7 +112,6 @@ self.port.on("okKey",function(keyString){
 
 $(window).on("blur focus", function(e) {
     var prevType = $(this).data("prevType");
-
     if (prevType != e.type) {   //  reduce double fire issues
         switch (e.type) {
             case "blur":
@@ -115,25 +123,24 @@ $(window).on("blur focus", function(e) {
                 break;
         }
     }
-
     $(this).data("prevType", e.type);
 });
 
 $(document).ready(function(){
-	CheckInterval();
 	ownID = getOwnId(ownID);
-	//ownID = sessionStorage.getItem("ownID");
 	if(ownID && !NSAcrypt.hasPrivateKey(ownID) && !sessionStorage.getItem("firstRun")){
-		console.log("firstrun");
 		sessionStorage.setItem("firstRun","true");
 		var date = new Date();
-		//TODO: Möglichkeit eigene Keys einzugeben
+		
+		//TODO: Möglichkeit eigene Keys 
+		
 		window.alert("Neue Keys werden generiert!\r\n\r\nDies kann einige Minuten dauern,\r\nFirefox bitte nicht beenden!\r\n\r\nZum starten OK klicken\r\n\r\n"+ date);
 		NSAcrypt.generateKeys(ownID);
+		self.port.emit("ownKeys",keys);
 		window.alert("Keys bitte sicher aufbewahren!");
 		window.alert(NSAcrypt.getPrivateKey(ownID));
 		window.alert(NSAcrypt.getPublicKey(ownID));
-		self.port.emit("loaded",true);
+		CheckInterval();
 	}else{
 		if(ownID && NSAcrypt.hasPrivateKey(ownID)){
 			var keys = [];
@@ -141,9 +148,7 @@ $(document).ready(function(){
 			keys[1] = NSAcrypt.getPublicKey(ownID);
 			self.port.emit("ownKeys",keys);
 			window.alert("Private Key found");
-			//window.alert("Private Key Found!");
-			//Kommentar entfernen zum testweise löschen der Keys
-			//NSAcrypt.clearKeys();
+			CheckInterval();
 		}
 	}
 });
@@ -164,20 +169,3 @@ function getOwnId(tempOwnID){
 	}
 	return tempOwnID;
 };
-
-function encrypt(s){
-    return (s ? s : this).split('').map(function(_)
-    {
-        if (!_.match(/[A-za-z]/)) return _;
-        c = Math.floor(_.charCodeAt(0) / 97);
-        k = (_.toLowerCase().charCodeAt(0) - 83) % 26 || 26;
-        return String.fromCharCode(k + ((c == 0) ? 64 : 96));
-    }).join('');
-}
-
-self.port.on("messageEncrypt",function(msgData){
-	console.log("bin drin");
-	var encrypted = encrypt(msgData[1]);
-	msgData[1] = encrypted;
-	self.port.emit("messageSend",msgData);
-});
